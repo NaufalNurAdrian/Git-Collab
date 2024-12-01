a002a17095744fd5f4e8fd20293cedbb226ee844
“NaufalNurAdrian” <nuradriannaufal@gmail.com>
2024-12-01 22:45:59 +0700

    fix 1.3

--- mini-project/src/app/dashboard/page.tsx
+++ mini-project/src/app/dashboard/page.tsx
@@ -2,15 +2,17 @@ export default function Dashboard() {
   return (
     <div className="container">
       <div className="w-screen flex flex-row">
-        <div className="flex flex-col w-[25%] h-screen bg-nobel p-2">
+        <div className="flex flex-col w-[20%] h-screen bg-nobel p-2">
           <div className="flex justify-center py-2 text-codgray font-extrabold text-3xl">
             <p className="text-red">Festify</p>Tix
           </div>
           <div className="font-bold">
             Dashboard
+            
           </div>
         </div>
-        <div className="flex w-[75%] bg-slate-200">
+
+        <div className="flex w-[80%] bg-slate-200">
             hallo
 
         </div>
